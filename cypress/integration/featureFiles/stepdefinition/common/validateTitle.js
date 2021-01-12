cb991d5360e07a281cf0f3cf2a5f1fbe1dbc4a3e
Mohit Agarwal <mohit.agarwal@nitorinfotech.com>
2021-01-12 20:47:57 +0530

    removing unwanted code and updating readme

--- cypress/integration/featureFiles/stepdefinition/common/validateTitle.js
+++ cypress/integration/featureFiles/stepdefinition/common/validateTitle.js
@@ -1,5 +1,5 @@
-import { Then } from "cypress-cucumber-preprocessor/steps";
+import { defineStep } from "cypress-cucumber-preprocessor/steps";
  
-Then(`I see {string} in the title`, (title) => {
+defineStep(`I see {string} in the title`, (title) => {
   cy.title().should('include', title)
 })
